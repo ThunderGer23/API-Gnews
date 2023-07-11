cb852e57b5e91a76a81a354b843bbf01435f2f2f
ThunderGer <jaames11@hotmail.com>
2023-07-11 17:24:09 -0600

    Agregando prueba para el test_AuthService

--- src/test/services/test_AuthService.py
+++ src/test/services/test_AuthService.py
@@ -10,3 +10,8 @@ def test_get_fullname_of_user_authenticated():
     _user = User(0, "ThunderGer", "abc123", None)
     authenticated_user = AuthService.login_user(_user)
     assert authenticated_user.fullname == 'Luis Gerardo Baeza'
+
+def test_get_username_of_user_authenticated():
+    _user = User(0, "ThunderGer", "abc123", None)
+    authenticated_user = AuthService.login_user(_user)
+    assert authenticated_user.username == 'ThunderGer'
