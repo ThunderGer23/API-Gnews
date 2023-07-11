8efe59e98f74b0b3036f2e0bda5a21741b77670e
ThunderGer <jaames11@hotmail.com>
2023-07-11 17:26:58 -0600

    Documentando pruebas

--- src/test/services/test_AuthService.py
+++ src/test/services/test_AuthService.py
@@ -2,16 +2,25 @@ from src.services.AuthService import AuthService
 from src.models.UserModel import User
 
 def test_get_authenticated_user_not_none():
+    """
+    The function tests that the authenticated user returned by the login_user function is not None.
+    """
     _user = User(0, "ThunderGer", "abc123", None)
     authenticated_user = AuthService.login_user(_user)
     assert authenticated_user != None
 
 def test_get_fullname_of_user_authenticated():
+    """
+    The function tests the functionality of retrieving the full name of an authenticated user.
+    """
     _user = User(0, "ThunderGer", "abc123", None)
     authenticated_user = AuthService.login_user(_user)
     assert authenticated_user.fullname == 'Luis Gerardo Baeza'
 
 def test_get_username_of_user_authenticated():
+    """
+    The function tests the ability to retrieve the username of an authenticated user.
+    """
     _user = User(0, "ThunderGer", "abc123", None)
     authenticated_user = AuthService.login_user(_user)
     assert authenticated_user.username == 'ThunderGer'
--- src/test/services/test_GNewsService.py
+++ src/test/services/test_GNewsService.py
@@ -3,16 +3,28 @@ from src.services.GNewsService import GNewsService
 
 
 def test_get_articles_not_none():
+    """
+    The function `test_get_articles_not_none` checks if the `articles` variable returned by the
+    `get_articles` method of the `GNewsService` class is not `None`.
+    """
     articles = GNewsService.get_articles()
     assert articles != None
 
 
 def test_get_articles_has_elements():
+    """
+    The function `test_get_articles_has_elements` checks if the `articles` list returned by the
+    `get_articles` method of the `GNewsService` class has elements.
+    """
     articles = GNewsService.get_articles()
     assert len(articles) > 0
 
 
 def test_get_articles_check_elements_length():
+    """
+    The function `test_get_articles_check_elements_length` checks if the length of each article obtained
+    from the GNewsService is greater than 0.
+    """
     articles = GNewsService.get_articles()
     for article in articles:
         assert len(article) > 0
